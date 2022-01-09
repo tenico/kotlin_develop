0a417474ff75c6e2c5dc1a8df296fdfd229369f4
David Silva <tenico94@gmail.com>
2022-01-09 22:15:18 +0000

    Roll Button APP _finished

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,16 +3,42 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
+
         rollButton.text=getString(R.string.main_button_text)
+
         rollButton.setOnClickListener {
-            Toast.makeText(this, "button clicked bro!", Toast.LENGTH_SHORT).show()
+          rollDice()
         }
+        diceImage =findViewById(R.id.dice_image)
+    }
+
+    private fun rollDice() {
+        val randomInt = Random.nextInt(until = 6) +1
+
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else ->R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+     /*   val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = randomInt.toString() */
     }
 }
